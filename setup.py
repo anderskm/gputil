83cba3e2cec1a65359da99545e766c61bcf47bb5
Anders Krogh Mortensen <anderskroghm@gmail.com>
2017-02-05 22:25:12 +0100

    Prepared repo for pypi.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+      name = 'GPUtil',
+      packages = ['GPUtil'],
+      version = '1.2.0',
+      description = '',
+      author = 'Anders Krogh Mortensen',
+      author_email = 'anderskroghm@gmail.com',
+      url = 'https://github.com/anderskm/gputil',
+      download_url = 'https://github.com/anderskm/gputil/tarball/v1.2.0',
+      keywords = ['gpu','utilization','load','memory','available','usage','free'],
+      classifiers = [],
+)
