e79361a97f0308c97157e50cc415b92f79ffa883
Mads Dyrmann <madsdyrmann@eng.au.dk>
2017-08-28 17:18:20 +0200

    Simplification

--- GPUtil/demo_GPUtil.py
+++ GPUtil/demo_GPUtil.py
@@ -1,4 +1,4 @@
-import __init__ as GPU
+import GPUtil as GPU
 
 # Get all device ids and their processing and memory utiliazion
 #(deviceIds, gpuUtil, memUtil) = GPU.getGPUs()
@@ -11,18 +11,23 @@ GPU.showUtilization()
 GPU.showUtilization(all=True)
 
 # Get all available GPU(s), ordered by ID in ascending order
+print('All available ordered by id: '),
 print(GPU.getAvailable(order = 'first', limit=999))
 
 # Get 1 available GPU, ordered by ID in descending order
-print(GPU.getAvailable(order = 'last'))
+print('Last available: '),
+print(GPU.getAvailable(order = 'last', limit = 1))
 
-# Get 1 available GPU, ordered randomly
+# Get 1 random available GPU
+print('Random available: '),
 print(GPU.getAvailable(order = 'random'))
 
-# Get 1 available GPU, ordered by load ascending
-print(GPU.getAvailable(order = 'load'))
+# Get 1 available GPU, ordered by GPU load ascending
+print('All available weighted by load ascending: '),
+print(GPU.getAvailable(order = 'load', limit = 1))
 
 # Get all available GPU with max load of 10%, ordered by memory ascending
+print('All available weighted by memory load ascending: '),
 print(GPU.getAvailable(order = 'memory', limit = 999, maxLoad=0.1))
 
 # Get the first available GPU
@@ -31,20 +36,20 @@ print('First available GPU id:'),
 print(firstGPU)
 
 # Get the first available GPU, where memory usage is less than 90% and processing is less than 80%
-print('First available GPU id (memory < 90%, load < 80%):'),
 firstGPU = GPU.getFirstAvailable(maxMemory = 0.9, maxLoad = 0.8)
+print('First available GPU id (memory < 90%, load < 80%):'),
 print(firstGPU)
 
 # Get the first available GPU, where processing is less than 1%
-print('First available GPU id (load < 1%):'),
 firstGPU = GPU.getFirstAvailable(attempts = 5, interval = 5, maxLoad = 0.01, verbose=True)
+print('First available GPU id (load < 1%):'),
 print(firstGPU)
 # NOTE: If all your GPUs currently have a load larger than 1%, this step will
 # fail. It's not a bug! It is intended to do so, if it does not find an available GPU.
 
 # Get the first available GPU, where memory usage is less than 1%
-print('First available GPU id (memory < 1%):'),
 firstGPU = GPU.getFirstAvailable(attempts = 5, interval = 5, maxMemory = 0.01, verbose=True)
+print('First available GPU id (memory < 1%):'),
 print(firstGPU)
 # NOTE: If all your GPUs currently have a memory consumption larger than 1%,
 # this step will fail. It's not a bug! It is intended to do so, if it does not
