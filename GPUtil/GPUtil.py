d0be047d7916f9953c71d1fd1fd4362bdd62f9d0
Istvan Fehervari <gooksl@gmail.com>
2018-05-16 11:12:54 -0700

    Added try-except around getgpu so the function does not crash on non-gpu hosts

--- GPUtil/GPUtil.py
+++ GPUtil/GPUtil.py
@@ -61,7 +61,10 @@ def safeFloatCast(strNumber):
 
 def getGPUs():
     # Get ID, processing and memory utilization for all GPUs
-    p = Popen(["nvidia-smi","--query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode", "--format=csv,noheader,nounits"], stdout=PIPE)
+    try:
+        p = Popen(["nvidia-smi","--query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode", "--format=csv,noheader,nounits"], stdout=PIPE)
+    except:
+        return []
     output = p.stdout.read().decode('UTF-8')
     # output = output[2:-1] # Remove b' and ' from string added by python
     #print(output)
