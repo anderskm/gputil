7271d3fca6ba0d233846db9bc04be2ae71238a71
SilviaAmAm <silvia.amabilino@bristol.ac.uk>
2018-10-05 12:06:39 +0100

    Modified the function that writes to a CSV file to output comma separated values for the ID, load and memory usage.

--- GPUtil/GPUtil.py
+++ GPUtil/GPUtil.py
@@ -296,127 +296,59 @@ def showUtilization(all=False, attrList=None, useOldCode=False):
             for GPUstring in GPUstrings:
                 print(GPUstring)
 
-def writeUtilization(all=False, attrList=None, useOldCode=False):
-    GPUs = getGPUs()
+def writeUtilization(delay=2, count=1):
+    """
+    Writes the stats of the GPU to a log file. Currently only takes the ID, the percentage load and the percentage
+    memory used,
 
-    f = open("gpu_log.out", "w")
+    :param delay: how often to write the GPU data to file in seconds
+    :type delay: float
+    :param count: how many times should the data be written to the file
+    :type count: int
+    """
 
-    if (all):
-        if (useOldCode):
-            f.write(
-                ' ID | Name | Serial | UUID || GPU util. | Memory util. || Memory total | Memory used | Memory free || Display mode | Display active |')
-            f.write(
-                '------------------------------------------------------------------------------------------------------------------------------')
-            for gpu in GPUs:
-                f.write(
-                    ' {0:2d} | {1:s}  | {2:s} | {3:s} || {4:3.0f}% | {5:3.0f}% || {6:.0f}MB | {7:.0f}MB | {8:.0f}MB || {9:s} | {10:s}'.format(
-                        gpu.id, gpu.name, gpu.serial, gpu.uuid, gpu.load * 100, gpu.memoryUtil * 100, gpu.memoryTotal,
-                        gpu.memoryUsed, gpu.memoryFree, gpu.display_mode, gpu.display_active))
-        else:
-            attrList = [[{'attr': 'id', 'name': 'ID'},
-                         {'attr': 'name', 'name': 'Name'},
-                         {'attr': 'serial', 'name': 'Serial'},
-                         {'attr': 'uuid', 'name': 'UUID'}],
-                        [{'attr': 'temperature', 'name': 'GPU temp.', 'suffix': 'C', 'transform': lambda x: x,
-                          'precision': 0},
-                         {'attr': 'load', 'name': 'GPU util.', 'suffix': '%', 'transform': lambda x: x * 100,
-                          'precision': 0},
-                         {'attr': 'memoryUtil', 'name': 'Memory util.', 'suffix': '%', 'transform': lambda x: x * 100,
-                          'precision': 0}],
-                        [{'attr': 'memoryTotal', 'name': 'Memory total', 'suffix': 'MB', 'precision': 0},
-                         {'attr': 'memoryUsed', 'name': 'Memory used', 'suffix': 'MB', 'precision': 0},
-                         {'attr': 'memoryFree', 'name': 'Memory free', 'suffix': 'MB', 'precision': 0}],
-                        [{'attr': 'display_mode', 'name': 'Display mode'},
-                         {'attr': 'display_active', 'name': 'Display active'}]]
-    else:
-        if (useOldCode):
-            f.write(' ID  GPU  MEM')
-            f.write('--------------')
-            for gpu in GPUs:
-                f.write(' {0:2d} {1:3.0f}% {2:3.0f}%'.format(gpu.id, gpu.load * 100, gpu.memoryUtil * 100))
-        else:
-            attrList = [[{'attr': 'id', 'name': 'ID'},
-                         {'attr': 'load', 'name': 'GPU', 'suffix': '%', 'transform': lambda x: x * 100, 'precision': 0},
-                         {'attr': 'memoryUtil', 'name': 'MEM', 'suffix': '%', 'transform': lambda x: x * 100,
-                          'precision': 0}],
-                        ]
+    f = open("gpu_log.csv", "w")
 
-    if (not useOldCode):
+    attrList = [[{'attr': 'id', 'name': 'ID'},
+                 {'attr': 'load', 'name': 'GPU', 'suffix': '%', 'transform': lambda x: x * 100, 'precision': 0},
+                 {'attr': 'memoryUtil', 'name': 'MEM', 'suffix': '%', 'transform': lambda x: x * 100,
+                  'precision': 0}],
+                ]
 
-        if (attrList is not None):
-            headerString = ''
-            GPUstrings = [''] * len(GPUs)
-            for attrGroup in attrList:
-                # print(attrGroup)
-                for attrDict in attrGroup:
-                    headerString = headerString + '| ' + attrDict['name'] + ' '
-                    headerWidth = len(attrDict['name'])
-                    minWidth = len(attrDict['name'])
 
-                    attrPrecision = '.' + str(attrDict['precision']) if ('precision' in attrDict.keys()) else ''
-                    attrSuffix = str(attrDict['suffix']) if ('suffix' in attrDict.keys()) else ''
-                    attrTransform = attrDict['transform'] if ('transform' in attrDict.keys()) else lambda x: x
-                    for gpu in GPUs:
-                        attr = getattr(gpu, attrDict['attr'])
+    headerString = ''
 
-                        attr = attrTransform(attr)
+    # Generating the header
+    for attrGroup in attrList:
 
-                        if (isinstance(attr, float)):
-                            attrStr = ('{0:' + attrPrecision + 'f}').format(attr)
-                        elif (isinstance(attr, int)):
-                            attrStr = ('{0:d}').format(attr)
-                        elif (isinstance(attr, str)):
-                            attrStr = attr;
-                        elif (sys.version_info[0] == 2):
-                            if (isinstance(attr, unicode)):
-                                attrStr = attr.encode('ascii', 'ignore')
-                        else:
-                            raise TypeError(
-                                'Unhandled object type (' + str(type(attr)) + ') for attribute \'' + attrDict[
-                                    'name'] + '\'')
+        for attrDict in attrGroup:
+            headerString += attrDict['name'] + ','
 
-                        attrStr += attrSuffix
+        f.write(headerString)
+        f.write("\n")
 
-                        minWidth = max(minWidth, len(attrStr))
+    # Extracting the GPU data
+    for counter in range(count):
 
-                    headerString += ' ' * max(0, minWidth - headerWidth)
+        GPUs = getGPUs()
+        GPUstrings = [''] * len(GPUs)
 
-                    minWidthStr = str(minWidth - len(attrSuffix))
+        for attrGroup in attrList:
 
-                    for gpuIdx, gpu in enumerate(GPUs):
-                        attr = getattr(gpu, attrDict['attr'])
+            for attrDict in attrGroup:
 
-                        attr = attrTransform(attr)
-
-                        if (isinstance(attr, float)):
-                            attrStr = ('{0:' + minWidthStr + attrPrecision + 'f}').format(attr)
-                        elif (isinstance(attr, int)):
-                            attrStr = ('{0:' + minWidthStr + 'd}').format(attr)
-                        elif (isinstance(attr, str)):
-                            attrStr = ('{0:' + minWidthStr + 's}').format(attr);
-                        elif (sys.version_info[0] == 2):
-                            if (isinstance(attr, unicode)):
-                                attrStr = ('{0:' + minWidthStr + 's}').format(attr.encode('ascii', 'ignore'))
-                        else:
-                            raise TypeError(
-                                'Unhandled object type (' + str(type(attr)) + ') for attribute \'' + attrDict[
-                                    'name'] + '\'')
-
-                        attrStr += attrSuffix
-
-                        GPUstrings[gpuIdx] += '| ' + attrStr + ' '
-
-                headerString = headerString + '|'
                 for gpuIdx, gpu in enumerate(GPUs):
-                    GPUstrings[gpuIdx] += '|'
+                    attr = getattr(gpu, attrDict['attr'])
 
-            headerSpacingString = '-' * len(headerString)
-            f.write(headerString)
-            f.write("\n")
-            f.write(headerSpacingString)
-            f.write("\n")
-            for GPUstring in GPUstrings:
-                f.write(GPUstring)
-                f.write("\n")
+                    attrStr = str(attr)
 
-            f.close()
+                    GPUstrings[gpuIdx] += attrStr + ','
+
+
+        for GPUstring in GPUstrings:
+            f.write(GPUstring)
+            f.write("\n")
+        # Wait before re-evaluating
+        time.sleep(delay)
+
+    f.close()
