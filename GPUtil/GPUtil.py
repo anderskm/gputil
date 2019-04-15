a4bf18d4fb84cdccc8bd4972c2dd345b6c5a06c6
Zijian Wang <zijwang@stanford.edu>
2019-04-14 17:23:20 -0700

    fix attrList issue

--- GPUtil/GPUtil.py
+++ GPUtil/GPUtil.py
@@ -234,7 +234,8 @@ def showUtilization(all=False, attrList=None, useOldCode=False):
             print('--------------')
             for gpu in GPUs:
                 print(' {0:2d} {1:3.0f}% {2:3.0f}%'.format(gpu.id, gpu.load*100, gpu.memoryUtil*100))
-        else:
+        elif attrList is None:
+            # if `attrList` was not specified, use the default one
             attrList = [[{'attr':'id','name':'ID'},
                          {'attr':'load','name':'GPU','suffix':'%','transform': lambda x: x*100,'precision':0},
                          {'attr':'memoryUtil','name':'MEM','suffix':'%','transform': lambda x: x*100,'precision':0}],
